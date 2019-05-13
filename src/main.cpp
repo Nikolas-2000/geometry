41f812321d85bfe96f0921331ac8b9b0fb9a9d4c
Nikita <nikolas_2000@mail.ru>
2019-05-13 15:21:47 +0700

    redacted main

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,21 @@ using namespace std;
 const float PI = 3.1415;
 int main()
 {
-    int a, b, c , r;
+    int a, b, c, r;
     float p, s;
 
-    cout << " Enter radius of circle : " ;
-    cin >> r ;
-    p = PerimetrC (r, PI);
-    s = SquareC (r, PI);
-    cout << " Perimetr : " << p << " Square : " << s << endl ;
+    cout << " Enter radius of circle : ";
+    cin >> r;
+    p = PerimetrC(r, PI);
+    s = SquareC(r, PI);
+    cout << " Perimetr : " << p << " Square : " << s << endl;
+
 
     cout << " Enter sidex of triangle : ";
-    cin >> a >> b >> c ;
-    p = PerimetrT (a,b,c);
-    s = SquareT (a,b,c);
-    cout << " Perimetr : " << p << " Square : " << s << endl ;
-   
+    cin >> a >> b >> c;
+    p = PerimetrT(a, b, c);
+    s = SquareT(a, b, c);
+    cout << " Perimetr : " << p << " Square : " << s << endl;
+
     return 0;
 }
