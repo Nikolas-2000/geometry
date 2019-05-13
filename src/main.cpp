4f34810373b02a5b9d7b98e4a2d19a161fac69d8
Nikita <nikolas_2000@mail.ru>
2019-05-13 15:15:53 +0700

    lab3

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "foo.h"
+#include <iostream>
+#include <math.h>
+using namespace std;
+const float PI = 3.1415;
+int main()
+{
+    int a, b, c , r;
+    float p, s;
+
+    cout << " Enter radius of circle : " ;
+    cin >> r ;
+    p = PerimetrC (r, PI);
+    s = SquareC (r, PI);
+    cout << " Perimetr : " << p << " Square : " << s << endl ;
+
+    cout << " Enter sidex of triangle : ";
+    cin >> a >> b >> c ;
+    p = PerimetrT (a,b,c);
+    s = SquareT (a,b,c);
+    cout << " Perimetr : " << p << " Square : " << s << endl ;
+   
+    return 0;
+}
