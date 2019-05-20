e5476c026768a6c93e264a704e84a41be7ca75d1
Nikita <nikolas_2000@mail.ru>
2019-05-20 15:09:36 +0700

    finish lab4

--- src/cross.c
+++ src/cross.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Cross(int x1, int y1, int r1, int x2, int y2, int r2)
+int cross(int x1, int y1, int r1, int x2, int y2, int r2)
 {
     int dis = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     if (dis <= (r1 + r2))
